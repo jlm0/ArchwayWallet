b1ed365f63e36246f0829588690324d247cc3b04
jlm0 <jordymcnab@kryptic.io>
2023-02-16 20:54:38 -0600

    Fix: App Name

--- android/app/src/main/java/com/kryptic/archwaywallet/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/kryptic/archwaywallet/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.archwaywallet.newarchitecture;
+package com.kryptic.archwaywallet.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.archwaywallet.BuildConfig;
-import com.archwaywallet.newarchitecture.components.MainComponentsRegistry;
-import com.archwaywallet.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.kryptic.archwaywallet.BuildConfig;
+import com.kryptic.archwaywallet.newarchitecture.components.MainComponentsRegistry;
+import com.kryptic.archwaywallet.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- android/app/src/main/java/com/kryptic/archwaywallet/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/kryptic/archwaywallet/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.archwaywallet.newarchitecture.components;
+package com.kryptic.archwaywallet.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
