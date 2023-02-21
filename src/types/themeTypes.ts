f640342f7d35a23d9a4a27053c95a4cbf3c5edaa
jlm0 <jordymcnab@kryptic.io>
2023-02-21 15:36:13 -0600

    Update: Theme type definition.
    1. Make the theme type definition more structured.

--- src/types/themeTypes.ts
+++ src/types/themeTypes.ts
@@ -7,146 +7,70 @@ export type AppThemesType = {
   };
 };
 
-type fontWeight = '400' | '500' | '800';
+export type fontWeight = '400' | '500' | '800';
+
+export type ThemeColors = {
+  primary: string;
+  onPrimary: string;
+  primaryContainer: string;
+  onPrimaryContainer: string;
+  secondary: string;
+  onSecondary: string;
+  secondaryContainer: string;
+  onSecondaryContainer: string;
+  tertiary: string;
+  onTertiary: string;
+  tertiaryContainer: string;
+  onTertiaryContainer: string;
+  error: string;
+  onError: string;
+  errorContainer: string;
+  onErrorContainer: string;
+  success: string;
+  onSuccess: string;
+  successContainer: string;
+  onSuccessContainer: string;
+  background: string;
+  onBackground: string;
+  surface: string;
+  onSurface: string;
+  surfaceVariant: string;
+  onSurfaceVariant: string;
+  outline: string;
+};
+
+export type ThemeElevation = {
+  surface: string;
+  surface1: string;
+  surface2: string;
+  surface3: string;
+  surface4: string;
+  surface5: string;
+};
+
+export type ThemeTypographyTypes = readonly [
+  'headline',
+  'display',
+  'title',
+  'label',
+  'body',
+];
+export type ThemeTypographySize = readonly ['small', 'medium', 'large'];
+export type ThemeTypographyProperties = {
+  fontFamily: string;
+  fontWeight: fontWeight;
+  lineHeight: number;
+  fontSize: number;
+};
+
+export type ThemeTypography = {
+  [K in ThemeTypographyTypes[number]]: {
+    [K in ThemeTypographySize[number]]: ThemeTypographyProperties;
+  };
+};
 
 export type ThemePropertiesType = {
-  colors: {
-    primary: string;
-    onPrimary: string;
-    primaryContainer: string;
-    onPrimaryContainer: string;
-    secondary: string;
-    onSecondary: string;
-    secondaryContainer: string;
-    onSecondaryContainer: string;
-    tertiary: string;
-    onTertiary: string;
-    tertiaryContainer: string;
-    onTertiaryContainer: string;
-    error: string;
-    onError: string;
-    errorContainer: string;
-    onErrorContainer: string;
-    success: string;
-    onSuccess: string;
-    successContainer: string;
-    onSuccessContainer: string;
-    background: string;
-    onBackground: string;
-    surface: string;
-    onSurface: string;
-    surfaceVariant: string;
-    onSurfaceVariant: string;
-    outline: string;
-  };
-  elevation: {
-    surface: string;
-    surface1: string;
-    surface2: string;
-    surface3: string;
-    surface4: string;
-    surface5: string;
-  };
-  typography: {
-    display: {
-      small: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      medium: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      large: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-    };
-    headline: {
-      small: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      medium: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      large: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-    };
-    title: {
-      small: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      medium: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      large: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-    };
-    body: {
-      small: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      medium: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      large: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-    };
-    label: {
-      small: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      medium: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-      large: {
-        fontFamily: string;
-        fontWeight: fontWeight;
-        lineHeight: number;
-        fontSize: number;
-      };
-    };
-  };
+  colors: ThemeColors;
+  elevation: ThemeElevation;
+  typography: ThemeTypography;
 };
